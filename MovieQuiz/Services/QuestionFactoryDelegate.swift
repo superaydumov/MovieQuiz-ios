a3295e7b31ff8d9b87987d221bd037d27694c2bc
Eldar Aidumov <superaydumov@me.com>
2023-06-08 01:47:38 +0300

    Refactored activity indicator animation logic. Worked with yes and no buttons logic (where they should't be active). Refactored main quiz question logic (now it's fully random with random rating index).

--- MovieQuiz/Services/QuestionFactoryDelegate.swift
+++ MovieQuiz/Services/QuestionFactoryDelegate.swift
@@ -6,10 +6,13 @@
 //
 
 import Foundation
+import UIKit
 
 protocol QuestionFactoryDelegate: AnyObject {
     func didReceiveNextQuestion (question: QuizQuestion?)
     
     func didLoadDataFromServer ()
     func didFailToLoadData (with error: Error)
+    
+    var activityIndicator: UIActivityIndicatorView! { get }
 }
