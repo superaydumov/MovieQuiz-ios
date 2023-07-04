28b1ee05c32460b0519471489b98e27330987e8d
Eldar Aidumov <superaydumov@me.com>
2023-07-05 00:35:28 +0300

    Added delay while the app is loading.

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -5,6 +5,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        Thread.sleep(forTimeInterval: 1.0)
         return true
     }
 
